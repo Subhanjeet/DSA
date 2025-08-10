0b239c1281f5757e9c791b0e22ef8c5bd1af4912
subhanjeet <ssubhanjeet@gmail.com>
2025-08-10 12:23:12 +0530

    initial commit

---
+++ 13.doubly_linkedlist_deletion_at_beginning.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+struct node *del_beg(struct node *head) {
+    if (head == NULL) {
+        printf("List is already empty.\n");
+        return NULL;
+    }
+    struct node *temp = head;
+    head = head->next;
+    if (head != NULL) {
+        head->prev = NULL;
+    }
+    free(temp);
+    return head;
+}
+
+int main() {
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 45;
+    head->prev = NULL;
+    head->next = NULL;
+
+    struct node *ptr = malloc(sizeof(struct node));
+    ptr->data = 18;
+    ptr->prev = head;
+    ptr->next = NULL;
+    head->next = ptr;
+
+    head = del_beg(head);
+
+    ptr = head;
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+
+    return 0;
+}
